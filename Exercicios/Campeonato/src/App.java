e20fac8f68eec67feb70d37a54dc367a22cf40f1
aluno <aluno@ftcbp.edu.br>
2022-05-18 11:32:31 -0300

    Aula 18 Maio

--- Exercicios/Campeonato/src/App.java
+++ Exercicios/Campeonato/src/App.java
@@ -24,6 +24,34 @@ public class App {
             soma = 0;
         }
 
+        System.out.println("\n\nDADOS PREENCHIDOS");
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println("\nNome: " + nomes[i]);
+            for (int j = 0; j < 4; j++) {
+                if (j == 3) {
+                    System.out.println("Pontuação Total: " + matriz[i][j]);
+                } else {
+                    System.out.println("Pontuação: " + matriz[i][j]);
+                }
+            }
+        }
+
+        int[][] aux = new int[1][4];
+
+        for (int i = 0; i < nomes.length - 1; i++) {
+            for (int j = i + 1; j < nomes.length; j++) {
+                if (matriz[i][3] < matriz[j][3]) {
+                    aux[0] = matriz[i];
+                    matriz[i] = matriz[j];
+                    matriz[j] = aux[0];
+                    String taux = nomes[i];
+                    nomes[i] = nomes[j];
+                    nomes[j] = taux;
+                }
+            }
+        }
+
+        System.out.println("\n\nDADOS ORDENADOS");
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("\nNome: " + nomes[i]);
             for (int j = 0; j < 4; j++) {
